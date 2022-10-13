5ef7523371ca41c876bada88c984be0ee615e9e0
Rodrigobi <112273897+Rodrigobi@users.noreply.github.com>
2022-10-12 19:30:15 -0500

    Mapa con boss y doble salto
    mecanica del boss, con el mapa y doble salto

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -19,7 +19,7 @@ public class EnemySpawner : MonoBehaviour
     private IEnumerator spawnEnemy (float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(130f, 135f), Random.Range(3f, 4.5f), 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(65f, 67f), Random.Range(0.8f, 2f), 0), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
     public void DestroyThings (GameObject EnemySpawner)
